3d78ed30fa15e309cab4a59cb367c37d1516b7cf
artem <artem28282828@gmail.com>
2023-05-22 10:54:40 +0500

    Первая задача

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число : ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+if (numberOne > numberTwo) 
+{
+    
+    Console.WriteLine("Число " + numberOne + " максимальное " );
+}
+else 
+{
+    Console.WriteLine("Число " + numberTwo + " максимальное ");
+}
+
