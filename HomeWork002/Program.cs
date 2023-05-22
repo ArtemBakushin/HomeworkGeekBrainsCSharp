3302b7efe0b04b29c11a91eb08ba17ea6bf9b8ec
artem <artem28282828@gmail.com>
2023-05-22 10:57:36 +0500

    Вторая задача

---
+++ HomeWork002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите первое число ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввидите второе число ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+int max = numberOne;
+if ( max > numberTwo )
+    if (max > numberThree)
+    Console.WriteLine("Число " + max + " максимальное");
+    else 
+    Console.WriteLine("Число " + numberThree + " максимальное");
+else 
+    if (numberTwo > numberThree)
+    {
+    Console.WriteLine("Число " + numberTwo + " максимальное");
+    }
+    else
+    Console.WriteLine("Число " + numberThree + " максимальное");
+
