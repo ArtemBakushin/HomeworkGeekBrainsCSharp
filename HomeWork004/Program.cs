3659ba9105a27c3a7c6055974148d9f04c45a310
artem <artem28282828@gmail.com>
2023-05-23 12:42:07 +0500

    Решение 4 Задачи но это не точно )))

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Write("Введите число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+ int N = 1 + 1 ; /*первое четное число после 1*/  
+ while ( N <= number )
+ {
+    Console.WriteLine(N);
+    N=N+2;
+ }
+
+
+ 
